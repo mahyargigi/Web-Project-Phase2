23392d4f4573f2695296807bdd2e4e9b974205ca
mahyargigi <mahyarghoraian@gmail.com>
2015-06-30 19:29:47 +0430

    follow-unfollow added

--- imgigi/accounts/views.py
+++ imgigi/accounts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
-
+from .models import UserProfile
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 # Create your views here.
+
+def user_profile(request , user_id):
+    user = UserProfile.objects.get(id=user_id)
+    yours , follows = False , False
+    if request.user.user == user :
+        yours = True
+    elif user in request.user.user.follows.all():
+        follows = True
+    return render(request , 'user-profile-yours.html' , {'user':user , 'yours':yours , 'follows':follows})
+
+@csrf_exempt
+def follow_unfollow(request):
+    subject_user = request.user.user
+    object_user = UserProfile.objects.get(id = request.POST.get('id'))
+    action = ""
+    if request.POST.get('action') == 'follow':
+        print('11')
+        action = "follow"
+    elif request.POST.get('action') == 'unfollow':
+        print('22')
+        action = "unfollow"
+    if action == "follow":
+        print('33')
+        if not object_user in subject_user.follows.all():
+            print('44')
+            subject_user.follows.add(object_user)
+    elif action == "unfollow":
+        print('55')
+        if object_user in subject_user.follows.all():
+            print('66')
+            subject_user.follows.remove(object_user)
+    return HttpResponse('done!')
--- imgigi/imgigi/urls.py
+++ imgigi/imgigi/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.http import HttpResponse
 from content.views import movie_profile , suggested_films
 from posts.views import timeline
+from accounts.views import user_profile , follow_unfollow
 from posts.views import post,post_comment, like
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,7 +22,7 @@ urlpatterns = [
     url(r'^signin/', temp_view),
     url(r'^forgot-pass/', temp_view),
     url(r'^signup/', temp_view),
-    url(r'users/(?:(?P<user_id>\d+)/)?$', temp_view),
+    url(r'users/(?:(?P<user_id>\d+)/)?$', user_profile),
     url(r'movies/(?:(?P<movie_id>\d+)/)?$', movie_profile),
     url(r'posts/(?:(?P<post_id>\d+)/)?$', post),
     url(r'^search/', temp_view),
@@ -29,4 +30,5 @@ urlpatterns = [
     url(r'^$', timeline),
     url(r'^post-comment', post_comment),
     url(r'^like', like),
+    url(r'^follow-unfollow', follow_unfollow),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
